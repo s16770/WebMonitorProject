56955a315d3df4aad5f4437cfee0f7f1f11c9127
Bartłomiej Kisil <s16770@pjwstk.edu.pl>
2021-02-05 12:55:47 +0100

    Device model update, html table populated

--- WebMonitorProject/WebMonitor/models.py
+++ WebMonitorProject/WebMonitor/models.py
@@ -6,7 +6,9 @@ class Device(models.Model):
     producent = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
     macaddress = models.CharField(max_length=20)
-    ipaddress = models.CharField(max_length=20)
+    ipaddress = models.GenericIPAddressField()
+    sessions = models.PositiveIntegerField()
+    status = models.NullBooleanField()
 
     def __str__(self):
         return self.name
--- WebMonitorProject/WebMonitor/views.py
+++ WebMonitorProject/WebMonitor/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Device
 
-devices = [
-    {
-        'Name': 'SW_Internal',
-        'Type': 'Switch',
-        'Vendor': 'Juniper',
-        'Model': 'EX2200'
-    },
-    {
-        'Name': 'ADServer',
-        'Type': 'Server',
-        'Vendor': 'Microsoft',
-        'Model': 'Windows Server 2019'
-    } 
-]
 
 def dashboard(request):
     context = {
-        'devices': devices
+        'devices': Device.objects.all()
     }
     return render(request, 'WebMonitor/dashboard.html', context)
 
