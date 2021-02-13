1af02e67eb70822e5caeb23dd29f3272c124d497
Bartłomiej Kisil <s16770@pjwstk.edu.pl>
2021-02-13 13:11:14 +0100

    login&logout

--- WebMonitorProject/WebMonitorProject/settings.py
+++ WebMonitorProject/WebMonitorProject/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # Add your apps here to enable them
     'WebMonitor.apps.WebMonitorConfig',
+    'Users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -112,3 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'webmonitorhome'
--- WebMonitorProject/WebMonitorProject/urls.py
+++ WebMonitorProject/WebMonitorProject/urls.py
@@ -19,8 +19,11 @@ Including another URLconf
 # Uncomment next two lines to enable admin:
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='Users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='Users/logout.html'), name='logout'),
     path('', include('WebMonitor.urls')),
 ]
