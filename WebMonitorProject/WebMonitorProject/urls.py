0941ad49848fd17eacb1b7bd579168caf5326ebe
Bartłomiej Kisil <s16770@pjwstk.edu.pl>
2021-04-11 11:58:52 +0200

    more tests

--- WebMonitorProject/WebMonitorProject/urls.py
+++ WebMonitorProject/WebMonitorProject/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('/admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='Users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='Users/logout.html'), name='logout'),
     path('', include('WebMonitor.urls')),
