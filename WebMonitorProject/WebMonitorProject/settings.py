5313b01053699ed6a26662a2390826d608b77b35
Bartłomiej Kisil <s16770@pjwstk.edu.pl>
2021-05-08 20:25:37 +0200

    testin

--- WebMonitorProject/WebMonitorProject/settings.py
+++ WebMonitorProject/WebMonitorProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '19811e48-4f9f-4353-90cc-7c3b9b3d5685'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.210.41.160']
+ALLOWED_HOSTS = ['*']
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
