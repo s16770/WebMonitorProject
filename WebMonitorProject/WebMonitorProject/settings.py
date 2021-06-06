2b6f4a35abcb41aec7841b1e8975582b943212e8
Bartłomiej Kisil <s16770@pjwstk.edu.pl>
2021-06-06 18:31:08 +0200

    email fix test

--- WebMonitorProject/WebMonitorProject/settings.py
+++ WebMonitorProject/WebMonitorProject/settings.py
@@ -121,6 +121,6 @@ LOGIN_REDIRECT_URL = 'webmonitorhome'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'webmonitor16770@gmail.com'
+EMAIL_HOST_USER = os.environ.get('webmonitorgmaillogin')
 EMAIL_HOST_PASSWORD = os.environ.get('webmonitorgmailpasswd')
 EMAIL_PORT = 587
