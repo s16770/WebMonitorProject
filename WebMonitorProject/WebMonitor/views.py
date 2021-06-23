219d584b5e7986f0678847b2b9c15d6364133e8b
Bartłomiej Kisil <s16770@pjwstk.edu.pl>
2021-06-23 12:11:38 +0200

    login required test

--- WebMonitorProject/WebMonitor/views.py
+++ WebMonitorProject/WebMonitor/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DeleteView
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Device
 from .models import Zone
@@ -7,7 +8,7 @@ from .models import Session
 from .models import Service
 from .models import Alert
 
-
+@login_required(login_url='/login/')
 def dashboard(request):
     context = {
         'devices': Device.objects.all()
